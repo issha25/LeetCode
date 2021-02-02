feced11d92670bee787e8b4ddbc783aa9b9394b4
Isha Gupta <68890096+issha25@users.noreply.github.com>
2021-02-02 18:10:28 +0530

    Create Next permutation.cpp

---
+++ Next permutation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(next_permutation(nums.begin(),nums.end()))
+           for(int i=0;i<nums.size();i++)
+            cout<<nums[i];
+    else{
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+            cout<<nums[i];
+        
+    }
+        
+        
+    }
+};
